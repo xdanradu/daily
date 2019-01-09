9ebe3bf2dc181a96082a82aa4b26910e07bd84fe
Dan RADU <vey2015@zzracnoe.rbgat.net>
2019-01-09 15:00:14 +0200

    up

--- js/storage.js
+++ js/storage.js
@@ -1,8 +1,11 @@
 function storage(){
+
     this.set=function(key, value){
         localStorage.setItem(key, JSON.stringify(value));
     }
+
     this.get=function(key){
         return JSON.parse(localStorage.getItem(key)) || [];
     }
+    
 }
